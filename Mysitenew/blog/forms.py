eccad42d174ee12e9e78aaf834310c0d30db1186
shaswat <kumarshaswat9@gmail.com>
2019-12-28 13:04:07 +0530

    first

---
+++ Mysitenew/blog/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+
+from .models import Post, Comment
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('author','title', 'text',)
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
+
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
+        }
+
+
+class LoginForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username','password',)
